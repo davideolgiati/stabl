7964c1ebef938343ddb7eb07429f2416a1ec6982
Davide Olgiati <davide.olgiati@hotmail.it>
2025-01-06 10:31:47 +0100

    Packages filter on severity and category + minor UI

--- stabl.py
+++ stabl.py
@@ -14,8 +14,10 @@ class updateEntry:
                 else:
                         self.tags.append('major')
                 
-                if (dnf_update_enrty['severity'] != "None"):
-                        self.tags.append(f"severity-{dnf_update_enrty['severity'].lower()}")
+                if (dnf_update_enrty['severity'] == "None"):
+                        self.tags.append("no-priority")
+                else:
+                        self.tags.append(f"{dnf_update_enrty['severity'].lower()}-priority")
 
 def run_shell_command(command_array):
         result = subprocess.run(command_array, stdout=subprocess.PIPE)
@@ -24,9 +26,38 @@ def run_shell_command(command_array):
 def process_update_list():
         output = run_shell_command(LIST_UPDATES)
         packages_list = json.loads(output)
+
+        updateGruops = {}
+        suggestedUpdates = []
+
         for package in packages_list:
                 current_package = updateEntry(package)
-                print(f"KEY: {current_package.key}\n\tPACKAGE_NAME: {current_package.packageName.ljust(60)}TAGS: [ {', '.join(current_package.tags)} ]")
+                if (current_package.key not in updateGruops):
+                        updateGruops[current_package.key] = [current_package]
+                else:
+                        updateGruops[current_package.key].append(current_package)
+
+        for key, packages in updateGruops.items():
+                print(f"{key}:")
+                addKey = False
+                securityProblem = False
+                for package in packages:
+                        print(f"\t{package.packageName.ljust(60)} [ {', '.join(package.tags)} ]")
+                        
+                        if('security' in package.tags or ('no-priority' not in package.tags)):
+                                securityProblem = True
+                        
+                        if('bugfix' in package.tags or securityProblem):
+                                addKey = True
+                        else:
+                                addKey = False
+                if(addKey):
+                        suggestedUpdates.append(key)
+        
+        if(suggestedUpdates != []):
+                print(f"suggested updates: sudo dnf update --advisory={','.join(suggestedUpdates)}")
+        else:
+                print(f"no suggested updates found") 
 
 
 if __name__ == "__main__":
