ae909dcd2e7ea737fbe2635e851863e8aa06bd5b
Davide Olgiati <davide.olgiati@hotmail.it>
2025-01-19 18:42:34 +0100

    shellInterface unit tests (happy path only)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # stabl
 A DNF wrapper to selectively choose what packages to upgrade
+
+### TODOs:
+
+- errors on wrong dnf entry format
+- manage errors on wrong dnf entry formats
+- errors on subprocess error
+- validation on subprocess output
+- validation on dnf update entry
+- better indexing for updates
---
+++ tests/test_ShellInterface.py
@@ -0,0 +1,31 @@
+from unittest.mock import MagicMock, patch
+#import pytest
+
+import tests.test_data.RawDNFOutput as RawDNFOutput
+
+from dao.ShellInterface import ShellInterface
+from common.costants import LIST_UPDATES_CMD
+
+
+@patch("dao.ShellInterface.subprocess.run")
+def test_get_data_valid(mock_run):
+    mock_stdout = MagicMock()
+    mock_stdout.configure_mock(
+        **{
+            "stdout.decode.return_value": RawDNFOutput.validDNFOutput
+        }
+    )
+
+    mock_run.return_value = mock_stdout
+
+    myShell = ShellInterface()
+
+    result = myShell.run(LIST_UPDATES_CMD)
+    assert result == RawDNFOutput.validDNFOutput
+
+
+# @patch("mypackage.proc.subprocess.run", side_effect=Exception("foobar"))
+# def test_get_data_invalid(mock_run):
+#     with pytest.raises(Exception) as exc:
+#         InternalProc.get_data()
+#         assert "foobar" in str(exc.value)
