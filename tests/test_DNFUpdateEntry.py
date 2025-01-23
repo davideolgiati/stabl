8c2fc0a06376140bf14633dd11fd8145c148a43c
Davide Olgiati <davide.olgiati@hotmail.it>
2025-01-23 20:47:37 +0100

    unit test DNFUpdateEntry error handling

--- tests/test_DNFUpdateEntry.py
+++ tests/test_DNFUpdateEntry.py
@@ -1,6 +1,7 @@
 # classes to be tested 
 import pytest
 from dto.DNFUpdateEntry import DNFUpdateEntry
+from dto.ManagedException import ManagedException
 from dto.UpdateClassification import UpdateClassification
 from dto.UpdateUrgency import UpdateUrgency
 
@@ -54,5 +55,23 @@ def test_DNFUpdateEntry_compare():
         with pytest.raises(TypeError):
                 assert("test" != test1)
 
+
+def test_DNFUpdateEntry_wrong_structure():
+        with pytest.raises(ManagedException) as e:
+                test1 = DNFUpdateEntry("pippo")
+        assert str(e.value) == "A Runtime error occurred while running stabl.py"
+        assert e.value.record == '\"pippo\"'
+        assert e.value.errors == ["Provided value is not an object"]
+        
+        with pytest.raises(ManagedException) as e:
+                test2 = DNFUpdateEntry({'ciao': 'test'})
+        assert str(e.value) == "A Runtime error occurred while running stabl.py"
+        assert e.value.record == '{"ciao": "test"}'
+        assert e.value.errors == [
+                "key 'name' is missing in provided record",
+                "key 'nevra' is missing in provided record",
+                "key 'type' is missing in provided record",
+                "key 'severity' is missing in provided record"
+        ]
 
 # TODO: mancano i test nel caso la struttura sia differente --> JSON schema validator
