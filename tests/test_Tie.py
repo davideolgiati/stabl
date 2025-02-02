a310d25fde3bd4d915c08a7dde5ea9aab8292a88
Davide Olgiati <davide.olgiati@hotmail.it>
2025-02-02 10:56:24 +0100

    Enhance Tie class with additional assertions for type and value validation; add lookup_prefix method and corresponding unit tests

--- tests/test_Tie.py
+++ tests/test_Tie.py
@@ -30,3 +30,15 @@ def test_2():
         assert my_tie.lookup_key("test2")
 
         assert len(my_tie) == 3
+
+def test_3():
+        my_tie = Tie()
+
+        my_tie.add_key("test1")
+        my_tie.add_key("key1")
+
+        assert my_tie.lookup_key("test1") 
+        assert my_tie.lookup_key("key1")
+        assert my_tie.lookup_prefix("test")
+        assert my_tie.lookup_prefix("key")
+        assert not my_tie.lookup_prefix("foo")
