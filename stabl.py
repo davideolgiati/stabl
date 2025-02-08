cce2f49b593b8608f771f20679ea5de3465b38d5
Davide Olgiati <davide.olgiati@hotmail.it>
2025-02-08 10:09:24 +0100

    Refactor update processing in stabl.py; separate advisory ID details and user message display into distinct functions for improved readability and maintainability

--- stabl.py
+++ stabl.py
@@ -1,26 +1,39 @@
 from dao.UpdateManager import UpdateManager
 from dao.DNF import DNF
 
-def process_update_list():
+def display_message_to_user(suggestedAdvisoryIds):
+    print()
+    if(suggestedAdvisoryIds != []):
+            advisories_string = ','.join(suggestedAdvisoryIds)
+            print(f"suggested updates: sudo dnf update --advisory={advisories_string}")
+    else:
+            print(f"no suggested updates found") 
+    print()
+
+def print_advisory_id_details(current_packages_list, advisoryId):
+        printBuffer = ""
+
+        for package in current_packages_list:
+                printBuffer += f"\t{package.packageName.ljust(60)}\n"
+                
+        print(f"Advisory Id: \"{advisoryId}\" \n{printBuffer}")
+
+def setup_package_mamager():
         packageManager = DNF()
         updateManager = UpdateManager(packageManager)
+        return updateManager
+
+def main():
+        updateManager = setup_package_mamager()
 
         packagesByAdvisoryId = updateManager.get_updates_by_advisory_id()
         suggestedAdvisoryIds = updateManager.get_suggested_advisory_ids()
         
         for advisoryId in suggestedAdvisoryIds:
-                printBuffer = ""
-                for package in packagesByAdvisoryId[advisoryId]:
-                        printBuffer += f"\t{package.packageName.ljust(60)}\n"
-                
-                print(f"Advisory Id: \"{advisoryId}\" \n{printBuffer}")
+                current_packages_list = packagesByAdvisoryId[advisoryId]
+                print_advisory_id_details(current_packages_list, advisoryId)
 
-
-        if(suggestedAdvisoryIds != []):
-                print(f"\nsuggested updates: sudo dnf update --advisory={','.join(suggestedAdvisoryIds)}\n")
-        else:
-                print(f"\nno suggested updates found\n") 
-
+        display_message_to_user(suggestedAdvisoryIds)
 
 if __name__ == "__main__":
-        process_update_list()
+        main()
