4a2280d963d8d4a86a0c97dcab49a0caaebc2288
Davide Olgiati <davide.olgiati@hotmail.it>
2025-02-25 22:09:58 +0100

    Refactor main logic: add extract_signature_list and extract_version_and_release_map functions; streamline update processing and improve clarity in main function

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use model::partitions::builder::PartitionBuilder;
 mod commons;
 use commons::string::split_string_using_delimiter;
 
+use std::collections::HashMap;
+
 fn display_stabl_logo() {
     let logo:&str = r"
      _        _     _ 
@@ -23,6 +25,37 @@ A DNF wrapper to selectively choose what packages to upgrade
     println!("{}", logo);
 }
 
+fn extract_signature_list(available_updates: Vec<String>) -> Vec<String> {
+    return available_updates
+        .into_iter()
+        .map(|line| split_string_using_delimiter(line, " "))
+        .map(|tokens| tokens[3].clone())
+        .collect();
+}
+
+fn extract_version_and_release_map(details_from_repository: Vec<String>) -> HashMap<String, (String, String, String)> {
+    let mut version_and_release_map = HashMap::new();
+    let details: Vec<Vec<String>> = details_from_repository
+        .clone()
+        .into_iter()
+        .map(|line| split_string_using_delimiter(line, "|#|"))
+        .collect();
+
+    for detail in details {
+        version_and_release_map.insert(
+            detail[4].clone(), 
+            (detail[0].clone(), detail[1].clone(), detail[2].clone())
+        );
+
+        version_and_release_map.insert(
+            detail[5].clone(), 
+            (detail[0].clone(), detail[1].clone(), detail[2].clone())
+        );
+    }
+
+    return version_and_release_map
+}
+
 fn main() {
     display_stabl_logo();
     let system_details:String = os::get_os_name();
@@ -35,21 +68,15 @@ fn main() {
     println!("[i] gruoping updates in partititons...");
     
     let mut partition_builder: PartitionBuilder = PartitionBuilder::new();
+    let signatures: Vec<String> = extract_signature_list(available_updates.clone());
+    let remote_details: Vec<String> = dnf::get_updates_details(signatures);
+    
     let updates: Vec<Update> = available_updates
                                 .into_iter()
                                 .map(|line| Update::from_dnf_output(line))
                                 .collect();
-    let signatures: Vec<String> = updates
-                                    .clone()
-                                    .into_iter()
-                                    .map(|update| update.get_signature().clone())
-                                    .collect();
-    let remote_details: Vec<String> = dnf::get_updates_details(signatures);
-    let processed_details: Vec<Vec<String>> = remote_details
-                                            .clone()
-                                            .into_iter()
-                                            .map(|line| split_string_using_delimiter(line, "|#|"))
-                                            .collect();
+
+    let processed_details: HashMap<String, (String, String, String)> = extract_version_and_release_map(remote_details.clone());
 
     for update in updates {
         partition_builder.register_update(update.clone());
