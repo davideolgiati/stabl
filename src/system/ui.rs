85edfea5f69dbe66849b1cccab4513f439bc68b8
Davide Olgiati <davide.olgiati@hotmail.it>
2025-03-07 16:51:37 +0100

    Refactor display_suggested_upgrades: reorder parameters for improved readability and maintainability

--- src/system/ui.rs
+++ src/system/ui.rs
@@ -22,7 +22,9 @@ pub fn display_system_informations() {
 pub fn display_suggested_upgrades(update_builder: &UpdateBuilder, buffer: String) {
         println!(
                 "{}\nMajor   updates: {}\nMinor   updates: {}\nPatch   updates: {}\nRelease updates: {}\n\n", 
-                buffer, update_builder.get_release_count(), update_builder.get_major_count(),
-                update_builder.get_minor_count(), update_builder.get_patch_count()
+                buffer, update_builder.get_major_count(),
+                update_builder.get_minor_count(), 
+                update_builder.get_patch_count(),
+                update_builder.get_release_count()
         );
 }
