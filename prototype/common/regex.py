a6bdc8acc60008fb4dacb27df3954f1c5e7b6d8b
Davide Olgiati <davide.olgiati@hotmail.it>
2025-03-29 10:15:58 +0100

    Removed prototype folder

--- prototype/common/regex.py
+++
@@ -1,6 +0,0 @@
-package_name = r'^[A-Za-z0-9\+]+([\-\_][A-Za-z0-9\+]+)*$'
-package_version = r'^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+){0,}$'
-
-valid_separator = r'\~|\^'
-
-valid_rpm_file = r'\.rpm$'
--- prototype/model/enum/UpdateClass.py
+++
@@ -1,9 +0,0 @@
-from model.enum.OrderedEnum import OrderedEnum
-
-
-class UpdateClass(OrderedEnum):
-        SECURITY = 0
-        RELEASE = 1
-        PATCH = 2
-        MINOR = 3
-        MAJOR = 4
